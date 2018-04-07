c0f3bfe4a487e55654fdad5788ba07ce37a2387c
workvictor <workvictor@yandex.ru>
2018-04-08 01:51:08 +0300

    add flow config

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,3 +1,4 @@
+// @flow
 import React from 'react';
 import styled from 'styled-components';
 
@@ -8,27 +9,36 @@ const Wrapper = styled.input`
   border: 1px solid #999;
 `
 
-export class Input extends React.Component {
-  state = {
-    value: ``,
-  }
+type Props = {
+  value: string,
+  onChange: ({value: string}) => void,
+}
 
-  onUserInput = ({ value }) => {
+type State = {|
+  value: string,
+|}
+
+export class Input extends React.Component<Props, State>{
+  state = {
+    value: ``
+  };
+
+  onUserInput = ({ value }:State) => {
     const { onChange } = this.props;
     this.onValueReceive({ value });
     onChange && onChange({ value });
   }
 
-  onValueReceive = ({ value }) => {
+  onValueReceive = ({ value }:State) => {
     value !== this.state.value && this.setState({ value });
   }
 
-  componentDidMount(){
+  componentDidMount() {
     const { value } = this.props;
     value && this.onValueReceive({ value });
   }
 
-  componentWillReceiveProps(nextProps) {
+  componentWillReceiveProps(nextProps: Props) {
     const { value } = nextProps;
     value !== this.props.value && this.onValueReceive({ value });
   }
